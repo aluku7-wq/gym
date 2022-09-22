3a9d58b136493ff9d6f5af1d8a3a33a74fc981c5
aluku godrick <foreveraluku@gmail.com>
2022-09-22 17:15:27 +0300

    update

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -8,11 +8,10 @@ const Logo = () => {
     align-items: baseline;
     gap: 8px;
     img {
-      filter: invert(30%) sepia(34%) saturate(3714%) hue-rotate(307deg)
-        brightness(90%) contrast(93%);
-      height: 40px;
+      height: 36px;
     }
     p {
+      margin-left: -24px;
       font-family: var(--oswald);
       font-weight: 700;
       font-size: 28px;
@@ -28,7 +27,7 @@ const Logo = () => {
   `;
   return (
     <Container>
-      <img src="/images/logo/logo.svg" alt="" />
+      <img src="/images/logo/logo.png" alt="" />
       <p>FITHUB</p>
       <span></span>
     </Container>
